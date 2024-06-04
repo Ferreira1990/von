060d4f74a4aec17d96c5b53e2c83c2baafa9ceb8
Cristian Ferreira <ferreira.8390mail.com>
2024-06-04 18:01:57 -0400

    video

--- src/components/body/Quienes.jsx
+++ src/components/body/Quienes.jsx
@@ -1,21 +1,38 @@
 import React from 'react'
 import '../../components/body/Quienes.css'
+import video from '../../assets/von.mp4'
 
 const Quienes = () => {
   return (
    <div id='quienes' className='Boxquienes'>
+
     <div className='Quienesuno'>
         <h1  data-aos="fade-right" data-aos-duration="2500">Quienes Somos?</h1>
     </div>
+
+    <div className='Boxofquienessomosvideo'>
+
     <div className='Quienesdos'>
-        <h4  data-aos="fade-left" data-aos-duration="2500"><span className='Spanvon'>Von Plötz</span> es una empresa
+        <h4  data-aos="zoom-in" data-aos-duration="2500"><span className='Spanvon'>Von Plötz</span> es una empresa
              que ofrece servicios de construcción,
               gasfitería, mueblería, pintura,sistemas 
               de regadío entre otros. Proporcionanamos soluciones completas 
               para construcciones, remodelaciones y reparaciones, además de servicios de instalación, 
               fabricación y pintura para hogares y empresas.</h4>
-
     </div>
+
+    <div className='Moviepresentation' >
+    <video data-aos="zoom-out" data-aos-duration="2500" src={video} autoPlay loop muted>
+
+    </video>
+  
+    </div>
+
+
+      
+      </div>  
+
+
 
    </div>
   )
